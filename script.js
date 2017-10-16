b557d646dc27260745632a9c1e230387ac3cfa5b
Marshall Elford <melford@alumni.ubc.ca>
2017-10-16 00:50:26 -0700

    Basic Layout

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-window.onload = function() {
-    var morning = "Good Morning!";
-    var afternoon = "Good Afternoon!";
-    var evening = "Good Evening!";
+window.onload = function () {
+    "use strict";
+    var morning = "Good Morning!", afternoon = "Good Afternoon!", evening = "Good Evening!";
   
   
     function getGreeting(date) {
         if (date.getHours() < 12) {
             return morning;
-        }
-        else if (date.getHours() >= 12 && date.getHours() < 18) {
+        } else if (date.getHours() >= 12 && date.getHours() < 18) {
             return afternoon;
-        }
-        else {
+        } else {
             return evening;
         }
     }
     
     document.getElementById("greeting").innerHTML = getGreeting(new Date());
-}
+};
